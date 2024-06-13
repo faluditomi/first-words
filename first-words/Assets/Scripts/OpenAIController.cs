f27f9fffcc2ce517aa00ced103b1995b6b7c29a0
hefty.dot <tomifaludi@gmail.com>
2024-06-13 22:51:18 +0200

    Michael Bolton is alive and flustered.

--- first-words/Assets/Scripts/OpenAIController.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "OpenAIController", menuName = "Scriptable Objects/OpenAIController")]
-public class OpenAIController : ScriptableObject
-{
-    
-}
